a4fc5e95cecd627738c769eaee9fcd58b587f9ec
Alromave <122586145+Alromave@users.noreply.github.com>
2024-07-28 15:47:55 -0600

    ejercicios del primer curso de programacíon

---
+++ name.py
@@ -0,0 +1,10 @@
+#This exercise ask the user his first and last name
+# then the programm print both names
+first_name  = input("What is your first name:  ")
+
+last_name = input ("What is your last name: ")
+
+print(f"Your name is {last_name.capitalize()}. {first_name.capitalize()} {last_name.capitalize()}")
+
+
+
